ba90a25f843f1940666d2039c4aae1cf765344e8
osama_abdellatif <155384353+MAD-SAM22@users.noreply.github.com>
2025-08-22 20:32:35 +0300

    added : header & players section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 
 function App() {
-  
+
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">player1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player-name">player2</span>
+            <span className="player-symbol">O</span>
+            <button>Edit</button>
+          </li>
+        </ol>
+        Game Board
+      </div>
+
+      Log
+    </main>
   )
 }
 
